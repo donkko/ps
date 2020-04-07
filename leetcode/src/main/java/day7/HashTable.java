d7d078e65826271b703b33aa7b68db4f1fd8092e
donkko <nudonkko@gmail.com>
2020-04-08 01:07:40 +0900

    Add LC day7

---
+++ leetcode/src/main/java/day7/HashTable.java
@@ -0,0 +1,22 @@
+package day7;
+
+import java.util.HashSet;
+
+// 너무 쉬운데 -_-
+public class HashTable {
+    public int countElements(int[] arr) {
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num : arr) {
+            hs.add(num);
+        }
+
+        int count = 0;
+        for (int num : arr) {
+            if (hs.contains(num + 1)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
