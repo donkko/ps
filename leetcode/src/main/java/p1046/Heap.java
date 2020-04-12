4992e802cad3b0b4d05e2e35aa881d2c6023e2bc
donkko <nudonkko@gmail.com>
2020-04-12 19:48:13 +0900

    Add LC #1046

---
+++ leetcode/src/main/java/p1046/Heap.java
@@ -0,0 +1,25 @@
+package p1046;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Heap {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(stones.length, Comparator.reverseOrder());
+        for (int stone : stones) {
+            pq.add(stone);
+        }
+
+        while (pq.size() > 1) {
+            int top1 = pq.poll();
+            int top2 = pq.poll();
+
+            if (top1 == top2) {
+                continue;
+            } else {
+                pq.add(top1 - top2);
+            }
+        }
+        return pq.peek();
+    }
+}
