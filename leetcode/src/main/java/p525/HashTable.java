4972bb5efc49848af103341955b0c808601877df
donkko <nudonkko@gmail.com>
2020-04-14 01:52:57 +0900

    Add LC #525 solution

--- leetcode/src/main/java/p525/HashTable.java
+++ leetcode/src/main/java/p525/HashTable.java
@@ -6,14 +6,22 @@ public class HashTable {
     public int findMaxLength(int[] nums) {
         int count = 0;
         int maxLength = 0;
-        //var indexMap = new HashMap<Integer, Integer>();
+        var indexMap = new HashMap<Integer, Integer>();
+        indexMap.put(0, -1);
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) count++;
             else count--;
 
-
+            if (indexMap.containsKey(count)) {
+                int length = i - indexMap.get(count);
+                if (length > maxLength) {
+                    maxLength = length;
+                }
+            } else {
+                indexMap.put(count, i);
+            }
         }
 
-        return 0;
+        return maxLength;
     }
 }
