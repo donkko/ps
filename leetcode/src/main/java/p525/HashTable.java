6cd38747c9eb9998fcb4544a575f4b145bb0a22e
donkko <nudonkko@gmail.com>
2020-04-14 01:37:31 +0900

    Modify module settings

---
+++ leetcode/src/main/java/p525/HashTable.java
@@ -0,0 +1,19 @@
+package p525;
+
+import java.util.HashMap;
+
+public class HashTable {
+    public int findMaxLength(int[] nums) {
+        int count = 0;
+        int maxLength = 0;
+        //var indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) count++;
+            else count--;
+
+
+        }
+
+        return 0;
+    }
+}
