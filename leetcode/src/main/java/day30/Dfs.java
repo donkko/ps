56afb22558dee16b12476602d7b8dda82a4067d6
donkko <nudonkko@gmail.com>
2020-05-01 18:18:10 +0900

    Add LC day30

---
+++ leetcode/src/main/java/day30/Dfs.java
@@ -0,0 +1,36 @@
+package day30;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+}
+
+public class Dfs {
+    int[] arr;
+
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+        this.arr = arr;
+        return dfs(root, 0);
+    }
+
+    private boolean dfs(TreeNode node, int currentPosition) {
+        if (currentPosition >= this.arr.length) {
+            return false;
+        }
+
+        if (node == null) {
+            return false;
+        }
+
+        if (node.val != this.arr[currentPosition]) {
+            return false;
+        }
+
+        if (node.left == null && node.right == null) {
+            return currentPosition == this.arr.length - 1;
+        } else {
+            return dfs(node.left, currentPosition + 1) || dfs(node.right, currentPosition + 1);
+        }
+    }
+}
